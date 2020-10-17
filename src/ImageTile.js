3d0d946cf8db89cb6754078db4a523d1d1f993da
iKoru <glassonyou@gmail.com>
2020-10-17 17:23:08 +0900

    updated selected tile styles

--- src/ImageTile.js
+++ src/ImageTile.js
@@ -1,33 +1,23 @@
-import React from 'react';
-import {
-  Dimensions,
-  ImageBackground,
-  TouchableHighlight,
-  View,
-} from 'react-native';
+import React from "react";
+import { Dimensions, ImageBackground, TouchableHighlight, View } from "react-native";
 
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get("window");
 
 class ImageTile extends React.PureComponent {
   render() {
     const { item, index, selected, selectImage, selectedItemNumber, renderSelectedComponent } = this.props;
     if (!item) return null;
     return (
-      <TouchableHighlight
-        style={{ opacity: selected ? 0.5 : 1 }}
-        underlayColor='transparent'
-        onPress={() => selectImage(index)} >
-        <View style={{ position: 'relative' }}>
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <ImageBackground
-              style={{ width: width / 4, height: width / 4 }}
-              source={{ uri: item.uri }} >
+      <TouchableHighlight underlayColor="transparent" onPress={() => selectImage(index)}>
+        <View style={{ position: "relative" }}>
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <ImageBackground style={{ width: width / 4, height: width / 4 }} source={{ uri: item.uri }}>
               {selected && renderSelectedComponent && renderSelectedComponent(selectedItemNumber)}
             </ImageBackground>
           </View>
         </View>
       </TouchableHighlight>
-    )
+    );
   }
 }
 
